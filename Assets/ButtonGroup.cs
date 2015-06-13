65254c613f198ea3f611ee8f71a3227f742b5396
Nathan DeGrand <degrand1@illinois.edu>
2015-06-12 23:20:31 -0700

    Initial commit
    Always banana displays text one at a time, has a therapist, and has four
    buttons

---
+++ Assets/ButtonGroup.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class ButtonGroup : MonoBehaviour {
+
+	public Button UpButton;
+	public Button DownButton;
+	public Button LeftButton;
+	public Button RightButton;
+
+	public void DisableButtons()
+	{
+		UpButton.gameObject.SetActive(false);
+		DownButton.gameObject.SetActive(false);
+		LeftButton.gameObject.SetActive(false);
+		RightButton.gameObject.SetActive(false);
+	}
+
+	public void EnableButtons()
+	{
+		UpButton.gameObject.SetActive(true);
+		DownButton.gameObject.SetActive(true);
+		LeftButton.gameObject.SetActive(true);
+		RightButton.gameObject.SetActive(true);
+	}
+
+	void Start() {
+		UpButton.onClick.AddListener(delegate { UpClicked(); } );
+		DownButton.onClick.AddListener(delegate { DownClicked(); } );
+		LeftButton.onClick.AddListener(delegate { LeftClicked(); } );
+		RightButton.onClick.AddListener(delegate { RightClicked(); } );
+	}
+
+	public void UpClicked() {
+		Debug.Log ("Up button clicked");
+	}
+
+	public void DownClicked() {
+		Debug.Log ("Down button clicked");
+	}
+
+	public void LeftClicked() {
+		Debug.Log ("Left button clicked");
+	}
+
+	public void RightClicked() {
+		Debug.Log ("Right button clicked");
+	}
+}
---
+++ Assets/UpdateLogic.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class UpdateLogic : MonoBehaviour {
+
+	public ButtonGroup Buttons;
+	public Text Content;
+	public string ProfessorName;
+	public string PatientName;
+	public float RPGTextDelay;
+
+	private int NumCharactersToDisplay;
+	private float CurrentTime;
+	private int CurrentStringLength;
+	private int CurrentData;
+
+	[System.Serializable] public class StateData {
+		public string Message;
+	}
+
+	public StateData[] Data;
+
+	// Use this for initialization
+	void Start () {
+		Buttons.DisableButtons();
+		NumCharactersToDisplay = 0;
+		CurrentTime = 0;
+		CurrentData = 0;
+		CurrentStringLength = Data[0].Message.Length;
+		Content.text = "";
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if( CurrentStringLength != NumCharactersToDisplay )
+		{
+			CurrentTime += Time.deltaTime;
+			if( CurrentTime >= RPGTextDelay )
+			{
+				NumCharactersToDisplay++;
+				Content.text = Data[CurrentData].Message.Substring( 0, NumCharactersToDisplay );
+				CurrentTime = 0.0f;
+				if( CurrentStringLength == NumCharactersToDisplay )
+				{
+					Buttons.EnableButtons ();
+				}
+			}
+		}
+	}
+}
