26c0a198df4f3a308980f5fd4df0064ded218fc5
Nathan DeGrand <degrand1@illinois.edu>
2015-06-13 20:14:10 -0700

    Moved scripts into the right folder, made the content width fit to the screen width

---
+++ Assets/Scripts/ExtendWidth.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ExtendWidth : MonoBehaviour {
+
+	RectTransform rectTransform;
+
+	void Awake() {
+		rectTransform = GetComponent<RectTransform>();
+	}
+
+	void Update() {
+		rectTransform.sizeDelta = new Vector2(Screen.width, rectTransform.rect.height);
+	}
+}
