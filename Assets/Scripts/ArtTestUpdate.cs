752a59da4c224f3d92043b44a5d9157c64e740c7
David Yu <lewen.yu@gmail.com>
2015-06-13 17:10:04 -0700

    fade to/from black using BlackScreen script
    - you must attach the black texture to it when you append the script to
      the main game logic object. A black texture is supplied in Sprites/

---
+++ Assets/Scripts/ArtTestUpdate.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ArtTestUpdate : MonoBehaviour {
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if ( Input.GetKey( "space" ) ) {
+			if ( GetComponent<BlackScreen>() != null ) {
+				GetComponent<BlackScreen>().FadeToBlack();
+			}
+		}
+
+		if ( Input.GetKey( "backspace" ) ) {
+			if ( GetComponent<BlackScreen>() != null ) {
+				GetComponent<BlackScreen>().FadeFromBlack();
+			}
+		}
+	}
+}
---
+++ Assets/Scripts/BlackScreen.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BlackScreen: MonoBehaviour {
+	public Texture blackTex;
+
+	private float alphaFadeValue;
+	public float fadeTimeSeconds; // seconds
+
+	private const float TO_BLACK = 1;
+	private const float FROM_BLACK = -1;
+	private const float HOLD = 0;
+	private float fadeDirection;
+
+	public delegate void DoneAction();
+	DoneAction doneAction;
+
+	// Use this for initialization
+	void Start () {
+		alphaFadeValue = 0;
+		fadeTimeSeconds = 2;
+		fadeDirection = HOLD;
+		doneAction = null;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+
+	// multiple times per frame
+	void OnGUI() {
+		alphaFadeValue += fadeDirection * Mathf.Clamp01( Time.deltaTime / fadeTimeSeconds );
+		Color oldColor = GUI.color;
+		GUI.color = new Color( 0, 0, 0, alphaFadeValue );
+		GUI.DrawTexture( new Rect(0, 0, Screen.width, Screen.height ), blackTex, ScaleMode.StretchToFill, true, 10.0F );
+		GUI.color = oldColor;
+
+		// hold state if overboard in either direction
+		if ( alphaFadeValue < 0 || alphaFadeValue > 1 ) {
+			fadeDirection = HOLD;
+			alphaFadeValue = Mathf.Clamp01( alphaFadeValue );
+			if ( doneAction != null ) {
+				doneAction();
+				doneAction = null;
+			}
+		}
+	}
+
+	public void FadeToBlack( DoneAction doneAction = null ) {
+		fadeDirection = TO_BLACK;
+		alphaFadeValue = 0;
+		this.doneAction = doneAction;
+	}
+
+	public void FadeFromBlack( DoneAction doneAction = null ) {
+		fadeDirection = FROM_BLACK;
+		alphaFadeValue = 1;
+		this.doneAction = doneAction;
+	}
+}
